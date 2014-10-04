6898ec594e461a2e273fbddd662d980d10ae15e3
Konstantin Lysenko <konstantin@atalanta-systems.com>
2014-10-05 02:56:58 +0700

    Modify: credentials environment variables names to suit skyscape.

--- ss_s3.rb
+++ ss_s3.rb
@@ -6,8 +6,8 @@ AWS::S3::Base.establish_connection!(
                                     :server            => 'cas00001.skyscapecloud.com',
                                     :port              => 8433,
                                     :use_ssl           => true,
-                                    :access_key_id     => ENV['AWS_ACCESS_KEY'],
-                                    :secret_access_key => ENV['AWS_SECRET_KEY']
+                                    :access_key_id     => ENV['SKYSCAPE_ACCESS_KEY'],
+                                    :secret_access_key => ENV['SKYSCAPE_SECRET_KEY']
                                     )
 
 AWS::S3::Service.buckets.each do |bucket|
